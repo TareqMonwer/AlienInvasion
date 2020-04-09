a75a52abf4e8284ae08bde41379adecf28c5a849
TareqMonwer <tareqmonwer137@gmail.com>
2020-04-10 05:00:34 +0600

    full fleet on screen

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ class Settings:
         """Initializes the game's settings."""
         # Screen settings
         self.screen_width = 1000
-        self.screen_height = 700
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
         # Ship settings
