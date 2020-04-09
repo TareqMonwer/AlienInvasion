22120fff3238995f9dadd32abfaf9ac33d77c0cd
TareqMonwer <tareqmonwer137@gmail.com>
2020-04-10 00:54:49 +0600

    first row of fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,7 +3,6 @@ from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
-from alien import Alien
 import game_functions as gf
 
 
@@ -15,18 +14,19 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # Make a ship
+    # Make a ship, group of bullets and a group of aliens.
     ship = Ship(ai_settings, screen)
-    # Make a group to store bullets in.
     bullets = Group()
+    aliens = Group()
 
-    # Make an alien.
-    alien = Alien(ai_settings, screen)
+    # Create the fleet of aliens.
+    gf.create_fleet(ai_settings, screen, aliens)
 
     # Start the main loop for the game.
     while True:
         # Watch for keyboard and mouse events.
         gf.check_events(ai_settings, screen, ship, bullets)
+
         # Ship move control
         ship.update()
 
@@ -34,7 +34,7 @@ def run_game():
         gf.update_bullets(bullets)
 
         # Redraw the screen during each pass through the loop.
-        gf.update_screen(ai_settings, screen, ship, alien, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
